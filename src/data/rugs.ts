779214889d30167c0b1926425be4b1ea7b3fb91a
haide <df.quintanillah@gmail.com>
2026-06-25 09:30:11 -0600

    Add touch swipe functionality to Lightbox component for improved user experience; update Phantom rug data to include additional image.

--- src/data/rugs.ts
+++ src/data/rugs.ts
@@ -4,9 +4,9 @@ export const phantomRug = {
   year: "2026",
   technique: "Tufting",
   images: [
+    "/rugs/phantom_rug/pic_sala_phantom_2.webp",
     "/rugs/phantom_rug/pic_studio_phantom.webp",
     "/rugs/phantom_rug/pic_sala_phantom.webp",
-    "/rugs/phantom_rug/pic_sala_phantom_2.webp",
     "/rugs/phantom_rug/pic_pared_phantom.webp",
   ],
 };
